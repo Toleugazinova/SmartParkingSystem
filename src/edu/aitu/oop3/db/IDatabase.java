3eafd3096bc961a2b1081de100507613e6f6b09b
Amina Toleugazinova <250272@astanait.edu.kz>
2026-01-24 14:06:47 +0500

    Amina Toleugazinova- IDatabase interface

---
+++ src/edu/aitu/oop3/db/IDatabase.java
@@ -0,0 +1,8 @@
+package edu.aitu.oop3.db;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface IDatabase {
+    Connection getConnection() throws SQLException, ClassNotFoundException;
+}
