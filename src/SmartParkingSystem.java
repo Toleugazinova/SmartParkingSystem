6eb0ed3d4406122d680cb05710c35519d58421af
Amina Toleugazinova <250272@astanait.edu.kz>
2026-01-24 19:28:12 +0500

    Amina Toleugazinova- SmartParkingSystem(console menu), Main class

--- src/SmartParkingSystem.java
+++ src/SmartParkingSystem.java
@@ -1,2 +1,55 @@
+import edu.aitu.oop3.db.DatabaseConnection;
+import interfaces.IDatabase;
+import repository.*;
+import service.*;
+import java.util.Scanner;
+
 public class SmartParkingSystem {
+    private final IDatabase db = new DatabaseConnection();
+    private final ParkingSpotRepository spotRepo = new ParkingSpotRepository(db);
+    private final TariffRepository tariffRepo = new TariffRepository(db);
+    private final VehicleRepository vehicleRepo = new VehicleRepository(db);
+    private final ReservationRepository resRepo = new ReservationRepository(db);
+    private final ReservationService resService = new ReservationService(spotRepo, vehicleRepo, tariffRepo, resRepo);
+    private final PricingService pricingService = new PricingService(resRepo, tariffRepo, spotRepo, vehicleRepo);
+    private final Scanner scanner = new Scanner(System.in);
+
+    public void start() {
+        while (true) {
+            System.out.println();
+            System.out.println("Smart Parking System");
+            System.out.println("1. Print all available spots");
+            System.out.println("2. Print tariffs");
+            System.out.println("3. Park vehicle");
+            System.out.println("4. Parking fee");
+            System.out.println("5. Quit");
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt(); scanner.nextLine();
+            try {
+                switch (choice) {
+                    case 1:
+                        spotRepo.printFreeSpots();
+                        break;
+                    case 2:
+                        tariffRepo.printAllTariffs();
+                        break;
+                    case 3:
+                        System.out.print("Choose spot: ");
+                        String p = scanner.nextLine();
+                        System.out.print("Type: ");
+                        String t = scanner.nextLine();
+                        System.out.println(resService.parkVehicle(p, t));
+                        break;
+                    case 4:
+                        System.out.print("Plate: ");
+                        p = scanner.nextLine();
+                        System.out.println(pricingService.calculateAndPay(p));
+                        break;
+                    case 5:
+                        return;
+                }
+            }
+            catch (Exception e) { System.out.println("Error: " + e.getMessage()); }
+        }
+    }
 }
