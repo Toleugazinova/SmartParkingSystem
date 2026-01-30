6cfe2be8dd776a56bd8f84082b604b9e0279cc17
Amina Toleugazinova <250272@astanait.edu.kz>
2026-01-30 16:48:22 +0500

    Amina Toleugazinova

--- src/edu/aitu/oop3/db/DatabaseConnection.java
+++ src/edu/aitu/oop3/db/DatabaseConnection.java
@@ -1,18 +1,39 @@
 package edu.aitu.oop3.db;
+
+import interfaces.IDatabase;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
 
 public class DatabaseConnection implements IDatabase {
     private static DatabaseConnection instance;
     private Connection connection;
 
+    private static final String URL = "jdbc:postgresql://aws-1-ap-northeast-1.pooler.supabase.com:5432/postgres?sslmode=require";
+    private static final String USER = "postgres.ycdxqgmrsyzfgojsbucb";
+    private static final String PASSWORD = loadPassword();
+
+    private static String loadPassword() {
+        Properties props = new Properties();
+        try (InputStream input = new FileInputStream("config.properties")) {
+            props.load(input);
+            String value = props.getProperty("DB_PASSWORD");
+            if (value == null || value.isBlank()) {
+                throw new RuntimeException("DB_PASSWORD is not set in config.properties");
+            }
+            return value;
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot load DB_PASSWORD from config.properties. Make sure the file exists.", e);
+        }
+    }
+
     private DatabaseConnection() {
         try {
-            String url = "jdbc:postgresql://aws-1-ap-northeast-1.pooler.supabase.com:5432/postgres";
-            String user = "postgres.ycdxqgmrsyzfgojsbucb";
-            String password = "Ne0DvSdLpK4URYNQ";
             Class.forName("org.postgresql.Driver");
-            this.connection = DriverManager.getConnection(url, user, password);
+            this.connection = DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (Exception e) {
             e.printStackTrace();
         }
